97bed6ebf4906dbdddfb917ec0f250e18d70cffa
samiemostafavi <samiemostafavi@gmail.com>
2022-08-14 20:16:46 +0200

    added gym multiprocessing example

--- qsimpy/core.py
+++ qsimpy/core.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field, make_dataclass
-from typing import Any, Callable, Dict, FrozenSet, List
+from typing import Any, Dict, FrozenSet, List
 
-import pandas as pd
-import polars as pl
 import simpy
-from pandas.api.types import is_bool_dtype, is_numeric_dtype, is_string_dtype
 from pydantic import BaseModel, PrivateAttr
 
 from .random import RandomProcess
@@ -412,123 +409,6 @@ class Node(Entity):
 
             if self.out is not None:
                 self._out.put(task)
-
-    def put(self, task):
-        self._store.put(task)
-
-
-def pandas_to_polars(pddf, pp_fn) -> pl.DataFrame:
-
-    # convert to pandas dataframe
-    # if pddf was a task
-    # pddf = pd.DataFrame([task])
-
-    # apply the function
-    if pp_fn is not None:
-        pddf = pp_fn(pddf)
-
-    # figure out the columns and datatypes
-    # the order is very important: bool is numeric too!
-    columns = []
-    for col in pddf.columns:
-        if is_bool_dtype(pddf[col]):
-            columns.append((col, pl.Boolean))
-        elif is_string_dtype(pddf[col]):
-            columns.append((col, pl.Utf8))
-        elif is_numeric_dtype(pddf[col]):
-            columns.append((col, pl.Float64))
-
-    return pl.DataFrame(
-        pddf, columns=columns
-    )  # [("col1", pl.Float32), ("col2", pl.Int64)]
-
-
-class PolarSink(Entity):
-    """The sink that receives all tasks and records it
-    in a Spark dataframe: dropped or finished
-    Spark provides the dataframe to save data larger than memory
-    Parameters
-    ----------
-    env : simpy.Environment
-        the simulation environment
-    debug : boolean
-        if true then the contents of each task will be printed as it is received.
-    """
-
-    type: str = "polarsink"
-    events: FrozenSet[str] = {"task_reception"}
-    attributes: Dict[str, Any] = {
-        "tasks_received": 0,
-    }
-    batch_size: int = (10000,)
-
-    _pl_received_tasks: pl.DataFrame = PrivateAttr()
-    _received_tasks: List[Task] = PrivateAttr()
-    _store: simpy.Store = PrivateAttr()
-    _post_process_fn: Callable = PrivateAttr()
-
-    def clean_attributes(self):
-        for att in self.attributes:
-            self.attributes[att] = 0
-
-    def prepare_for_run(self, model: Model, env: simpy.Environment, debug: bool):
-        self._model = model
-        self._env = env
-        self._debug = debug
-
-        if self.out is not None:
-            self._out = model.entities[self.out]
-        if self.drop is not None:
-            self._drop = model.entities[self.drop]
-
-        self._store = simpy.Store(env)
-        self._received_tasks = []
-        self._pl_received_tasks = None
-
-        self._action = model._env.process(
-            self.run()
-        )  # starts the run() method as a SimPy process
-
-    def run(self):
-        while True:
-            task = yield self._store.get()
-
-            # EVENT task_reception
-            task = self.add_records(task=task, event_name="task_reception")
-
-            self.attributes["tasks_received"] += 1
-            if self._debug:
-                print(task)
-
-            # save the received task into the pandas dataframe
-            self._received_tasks.append(task)
-
-            if len(self._received_tasks) >= self.batch_size:
-                pddf = pd.DataFrame(self._received_tasks)
-                # save the received task into a Polars dataframe
-                if self._pl_received_tasks is None:
-                    self._pl_received_tasks = pandas_to_polars(
-                        pddf, self._post_process_fn
-                    )
-                else:
-                    self._pl_received_tasks = self._pl_received_tasks.vstack(
-                        pandas_to_polars(pddf, self._post_process_fn)
-                    )
-                del task, pddf
-                self._received_tasks = []
-
-    @property
-    def received_tasks(self):
-        pddf = pd.DataFrame(self._received_tasks)
-        if self._pl_received_tasks is None:
-            self._pl_received_tasks = pandas_to_polars(pddf, self._post_process_fn)
-        else:
-            self._pl_received_tasks = self._pl_received_tasks.vstack(
-                pandas_to_polars(pddf, self._post_process_fn)
-            )
-        del pddf
-        self._received_tasks = []
-        return self._pl_received_tasks
 
     def put(self, task):
         self._store.put(task)
---
+++ qsimpy/gym.py
@@ -0,0 +1,176 @@
+from __future__ import annotations
+
+from dataclasses import field, make_dataclass
+from typing import Any, Dict, FrozenSet
+
+import pandas as pd
+import simpy
+from pydantic import PrivateAttr
+
+from .core import Entity, Model, Task
+from .polar import PolarSink, pandas_to_polars
+from .utils import get_all_values
+
+
+class GymSource(Entity):
+    """Generates gym tasks
+    Set the "out" member variable to the entity to receive the task.
+
+    Parameters
+    ----------
+    env : Environment
+        the QSimPy simulation environment
+    main_task_type : str
+        type of the tasks being generated
+    traffic_task_type : str
+        type of the tasks being generated
+    traffic_task_num : int
+        the number of traffic tasks to go before the main task
+    initial_delay : number
+        Starts task generation after an initial delay. Default = 0
+    finish_time : number
+        Stops generation at the finish time. Default is infinite
+    """
+
+    type: str = "gymsource"
+    events: FrozenSet[str] = {"task_generation"}
+    attributes: Dict[str, Any] = {
+        "main_tasks_generated": 0,
+        "traffic_tasks_generated": 0,
+    }
+    main_task_type: str
+    traffic_task_type: str
+    traffic_task_num: int
+    initial_delay: float = 0
+    finish_time: float = None
+
+    _store: simpy.Store = PrivateAttr()
+
+    def prepare_for_run(self, model: Model, env: simpy.Environment, debug: bool):
+        self._model = model
+        self._env = env
+        self._debug = debug
+
+        if self.out is not None:
+            self._out = model.entities[self.out]
+        if self.drop is not None:
+            self._drop = model.entities[self.drop]
+
+        self._store = simpy.Store(env)
+        # starts the run() method as a SimPy process
+        self._action = model._env.process(self.run())
+
+    def clean_attributes(self):
+        for att in self.attributes:
+            self.attributes[att] = 0
+
+    def generate_main_task(self):
+
+        new_task = Task(
+            id=self.attributes["main_tasks_generated"],
+            task_type=self.main_task_type,
+        )
+
+        # create a GeneratedTask dataclass with the fields that come from the
+        # timestamps and attributes form the fields for make_dataclass
+        if self._model.task_records:
+            fields = [
+                (name, float, field(default=-1))
+                for name in get_all_values(self._model.task_records)
+            ]
+            # call make_dataclass
+            new_task.__class__ = make_dataclass(
+                "GeneratedTask", fields=fields, bases=(Task,)
+            )
+
+        self.attributes["main_tasks_generated"] += 1
+
+        return new_task
+
+    def generate_traffic_tasks(self, n: int):
+
+        traffic_tasks = [
+            Task(
+                id=self.attributes["traffic_tasks_generated"],
+                task_type=self.traffic_task_type,
+            )
+            for _ in range(n)
+        ]
+
+        self.attributes["traffic_tasks_generated"] += n
+
+        return traffic_tasks
+
+    def run(self):
+        """The generator function used in simulations."""
+        yield self._env.timeout(self.initial_delay)
+        if self.finish_time is None:
+            _finish_time = float("inf")
+        while self._env.now < _finish_time:
+            # generate and send the traffic
+            traffic_tasks = self.generate_traffic_tasks(self.traffic_task_num)
+            if self.out is not None:
+                for traffic_task in traffic_tasks:
+                    self._out.put(traffic_task)
+
+            # generate and send the main task
+            main_task = self.generate_main_task()
+            # EVENT task_generation
+            main_task = self.add_records(task=main_task, event_name="task_generation")
+
+            if self._debug:
+                print(main_task)
+
+            if self.out is not None:
+                self._out.put(main_task)
+
+            # wait for the next transmission
+            yield self._store.get()
+
+    def put(self, start_msg):
+        self._store.put(start_msg)
+
+
+class GymSink(PolarSink):
+    """
+    A sink entity that drops traffic tasks
+    """
+
+    type: str = "gymsink"
+
+    def put(self, task: Task):
+        if task.task_type == "main":
+            self._store.put(task)
+        else:
+            del task
+
+    def run(self):
+        while True:
+            task = yield self._store.get()
+
+            # EVENT task_reception
+            task = self.add_records(task=task, event_name="task_reception")
+
+            self.attributes["tasks_received"] += 1
+            if self._debug:
+                print(task)
+
+            self._received_tasks.append(task)
+
+            if len(self._received_tasks) >= self.batch_size:
+                pddf = pd.DataFrame(self._received_tasks)
+                # save the received task into a Polars dataframe
+                if self._pl_received_tasks is None:
+                    self._pl_received_tasks = pandas_to_polars(
+                        pddf, self._post_process_fn
+                    )
+                else:
+                    self._pl_received_tasks = self._pl_received_tasks.vstack(
+                        pandas_to_polars(pddf, self._post_process_fn)
+                    )
+                del task, pddf
+                self._received_tasks = []
+
+            if self.out is not None:
+                # send the start message to the source
+                self._out.put(Task(id=0, task_type="start_msg"))
